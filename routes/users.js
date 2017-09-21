e73e768034c45b2b42b6046de9fac5b6049b73d9
liwwww <a15992997992@163.com>
2017-09-21 15:53:50 +0800

    注释了代码，看实际情况编写

--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,4 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/add',function(req, res, next) {
-  userDao.add(req, res, next);
-});
-
 module.exports = router;
